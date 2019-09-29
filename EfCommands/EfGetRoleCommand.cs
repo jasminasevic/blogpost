0955b9a6cff611e6da3a0880cf1d99ba96b6e07f
jasminasevic <jasminasevic@yahoo.com>
2019-09-29 22:52:40 +0200

    Improve get one role command

--- EfCommands/EfGetRoleCommand.cs
+++ EfCommands/EfGetRoleCommand.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Text;
 using EfDataAccess;
 using Application.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace EfCommands
 {
@@ -16,14 +18,23 @@ namespace EfCommands
 
         public ShowRoleDto Execute(int request)
         {
-            var role = Context.Roles.Find(request);
+            var role = Context.Roles
+                .Include(u => u.Users)
+                .Where(r => r.Id == request)
+                .FirstOrDefault();
 
             if (role == null)
                 throw new NotFoundException();
 
             return new ShowRoleDto
             {
-                Name = role.Name
+                Name = role.Name,
+                BasicUserInfoDtos = role.Users.Select(u => new BasicUserInfoDto
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Username = u.Username
+                })
             };
 
         }
